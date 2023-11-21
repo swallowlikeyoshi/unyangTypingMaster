f7333dbd7c905aa7fa597772d9975faace9fd33b
Dohyeon Kim <xingchen8837@gmail.com>
2023-11-22 02:16:31 +0900

    first

---
+++ main.py
@@ -0,0 +1,145 @@
+import os
+import sqlite3
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+import tkinter as tk
+from tkinter import simpledialog
+import time
+
+# SQLite3 데이터베이스 연결~
+conn = sqlite3.connect('ranking.db')
+cursor = conn.cursor()
+
+# 테이블 생성
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        grade INTEGER,
+        class_num INTEGER,
+        number INTEGER,
+        name TEXT,
+        rstDate TEXT,
+        rstMaxSpd INTEGER,
+        rstAvgSpd INTEGER,
+        rstAvgAcc INTEGER
+    )
+''')
+conn.commit()
+
+# 웹 드라이버 설정
+service = Service(excutable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+def challenge_typing():
+
+    # 사용자 정보 입력
+    grade = int(input("학년을 입력하세요: "))
+    class_num = int(input("반을 입력하세요: "))
+    number = int(input("번호를 입력하세요: "))
+    name = input("이름을 입력하세요: ")
+
+    # 중복 체크
+    cursor.execute('''
+        SELECT id FROM users
+        WHERE grade = ? AND class_num = ? AND number = ?
+    ''', (grade, class_num, number))
+    existing_user = cursor.fetchone()
+    if existing_user:
+        print("이미 도전한 사용자입니다.")
+        return
+    else:
+        print("도전을 시작합니다.")
+        time.sleep(2)
+
+    # 웹사이트로 이동
+    driver.implicitly_wait(3000)
+    driver.get('https://typing.works/')
+    tws_result = driver.find_element(By.CLASS_NAME, "tws-result-on")
+
+    rstDate = driver.find_element(By.ID, "rstDate").text
+
+    rstSpd = driver.find_element(By.ID, "rstAvgSpd").text
+    idx = rstSpd.index('/')
+    rstMaxSpd = rstSpd[0:idx]
+    rstAvgSpd = rstSpd[idx + 1:]
+
+    rstAvgAcc = driver.find_element(By.ID, "rstAvgAcc").text
+
+    driver.close()
+    
+    # 데이터베이스에 사용자 정보 저장
+    cursor.execute('''
+        INSERT INTO users (grade, class_num, number, name, rstDate, rstMaxSpd, rstAvgSpd, rstAvgAcc)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (grade, class_num, number, name, rstDate, rstMaxSpd, rstAvgSpd, rstAvgAcc))
+    conn.commit()
+
+    # GUI로 결과 표시
+    show_result_gui(name, rstAvgSpd, rstAvgAcc)
+
+def show_ranking():
+    # 랭킹 조회
+    cursor.execute('''
+        SELECT grade, class_num, number, name, rstAvgSpd, rstAvgAcc
+        FROM users
+        ORDER BY rstAvgSpd DESC
+        LIMIT 10
+    ''')
+    ranking = cursor.fetchall()
+
+    if not ranking:
+        print("아무도 없음")
+    else:
+        print("랭킹:")
+        for i, (grade, class_num, number, name, rstAvgSpd, rstAvgAcc) in enumerate(ranking, 1):
+            print(f"{i}. {grade}{str(class_num).zfill(2)}{str(number).zfill(2)} - {name} - 평균: {rstAvgSpd} WPM, 정확도: {rstAvgAcc}%")
+
+def show_result_gui(name, rstAvgSpd, avg_acc):
+    # 결과를 GUI로 표시
+    root = tk.Tk()
+    root.title("타자 결과")
+
+    root.geometry("400x300")
+    root.lift()
+    root.attributes('-topmost', True)
+
+    label1 = tk.Label(root, text="평균 속도: " + str(rstAvgSpd) + " WPM")
+
+    label2 = tk.Label(root, text="평균 정확도: " + str(avg_acc) + "%")
+
+    
+    label1.place(relx=0.5, rely=0.4, anchor='center')
+    label2.place(relx=0.5, rely=0.5, anchor='center')
+
+    confirm_button = tk.Button(root, text="확인", command=root.destroy)
+    confirm_button.place(relx=0.5, rely=0.8, anchor='center')
+
+    root.mainloop()
+
+def close():
+    # 연결 종료
+    print('프로그램을 종료합니다.')
+    conn.close()
+    driver.quit()
+    exit()
+
+# 메인 콘솔 창
+while True:
+    os.system('cls')
+    print("----------운양고 타자왕----------")
+    show_ranking()
+
+    choice = input("도전하려면 Y를 누르세요: ")
+    
+    if choice.lower() == 'y':
+        challenge_typing()
+    elif choice.lower() == 'rrrrrr':
+        show_ranking()
+    elif choice.lower() == 'qqqqqq':
+        close()
+    else:
+        continue
